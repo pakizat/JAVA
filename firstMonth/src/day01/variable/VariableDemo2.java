171735d8bd45dacc8271911cb1f012f67d8da64d
pakizat <1120669238@qq.com>
2025-02-27 11:26:01 +0800

    完成 day01 学习

---
+++ firstMonth/src/day01/variable/VariableDemo2.java
@@ -0,0 +1,31 @@
+package day01.variable;
+
+public class VariableDemo2 {
+    public static void main(String[] args) {
+        printVariable2();
+    }
+    public static void printVariable2(){
+
+        //定义八种基本数据类型变量
+        byte b = 10;
+        short s = 10;
+        int i = 10;
+        long l = 10;
+        float f = 10.0f;
+        double d = 10.0;
+        char c = 'a';
+        boolean bl = true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bl);
+
+        System.out.println("========================================");
+
+    }
+}
+
