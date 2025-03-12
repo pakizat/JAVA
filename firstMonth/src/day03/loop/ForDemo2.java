76808fe3d99188831a38d16f493a13485a6914dd
pakizat <1120669238@qq.com>
2025-03-12 17:09:10 +0800

    25.3.12 学习进度

---
+++ firstMonth/src/day03/loop/ForDemo2.java
@@ -0,0 +1,54 @@
+package day03.loop;
+
+import java.util.Scanner;
+
+/**
+ * 使用for 循环 选择出 一定范围内的 奇偶数
+ */
+public class ForDemo2 {
+    public static void main(String[] args) {
+        System.out.println("请输入 1 或 2 挑出 奇数或偶数");
+        Scanner sc = new Scanner(System.in);
+        int flag = sc.nextInt();
+        System.out.println("请分别输入 取值范围");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a < b) {
+            if (flag == 1) {
+                oddNum(a, b);
+            } else if (flag == 2) {
+                evenNum(a, b);
+            } else {
+                System.out.println("输入有误 程序退出");
+            }
+        }else{
+            System.out.println("输入有误 程序退出");
+        }
+    }
+
+    //1.挑出范围内的奇数
+    public static void oddNum(int a, int b) {
+        int c = 0;
+        if (a % 2 == 1) {
+            c = a;
+        } else {
+            c = a + 1;
+        }
+        for (int d = c; d <= b; d += 2) {
+            System.out.println(d);
+        }
+
+    }
+    public static void evenNum(int a, int b) {
+        int c = 0;
+        if (a % 2 == 0) {
+            c = a;
+        } else {
+            c = a + 1;
+        }
+        for (int d = c; d <= b; d += 2) {
+            System.out.println(d);
+        }
+
+    }
+}
---
+++ firstMonth/src/day03/loop/WhileDemo4.java
@@ -0,0 +1,23 @@
+package day03.loop;
+
+/**
+ * while 使用案例
+ */
+public class WhileDemo4 {
+    public static void main(String[] args) {
+        demo1();
+    }
+
+    //1.珠穆朗玛峰 高8848.86m 即换算成毫米为8848860  一张纸的厚度为 0.1毫米 问纸张这得多少次 可以 达到珠峰高度
+    public static void demo1(){
+        int i = 0;
+        double height = 0;
+        while(height < 8848860){
+            i++;
+            height = 0.1 * Math.pow(2,i);
+            Double a = height;
+
+        }
+        System.out.println(i);
+    }
+}
