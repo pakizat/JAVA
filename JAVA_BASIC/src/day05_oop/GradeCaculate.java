21a1ccd87215f60b72287f7e8427dd1a0913b5f9
pakizat <1120669238@qq.com>
2025-03-18 17:39:51 +0800

    2025.03.18 下班

---
+++ JAVA_BASIC/src/day05_oop/GradeCaculate.java
@@ -0,0 +1,50 @@
+package src.day05_oop;
+
+/**
+ * 目标 使用类的属性和方法的封装 实现学生成绩计算
+ */
+public class GradeCaculate {
+    public static void main(String[] args) {
+        //1.创建第一个学生对象 并输入名字和成绩
+        Student stu1 = new Student();
+        stu1.name = "博妞";
+        stu1.mathScore = 100;
+        stu1.englishScore = 100;
+        stu1.sex = "男";
+        stu1.age = 18;
+        stu1.totalScore();
+        stu1.averageScore();
+
+        //2.创建第二个学生对象 并输入名字和成绩
+        Student stu2 = new Student();
+        stu2.name = "博仔";
+        stu2.mathScore = 59;
+        stu2.englishScore = 100;
+        stu2.sex = "女";
+        stu2.age = 19;
+        stu2.totalScore();
+        stu2.averageScore();
+
+//        //普通的方法构造
+//        //1.打印总成绩
+//        totalScore(stu1);
+//        totalScore(stu2);
+//
+//        //2.打印平均成绩
+//        averageScore(stu1);
+//        averageScore(stu2);
+
+
+    }
+//    //普通的方法构造
+//    //总成绩计算函数
+//    public static void totalScore(Student stu){
+//        System.out.println(stu.name + "的总成绩是：" + (stu.mathScore + stu.englishScore));
+//    }
+//
+//    //平均成绩计算函数
+//    public static void averageScore(Student stu){
+//        System.out.println(stu.name + "的平均成绩是：" + (stu.mathScore + stu.englishScore) / 2);
+//    }
+
+}
---
+++ JAVA_BASIC/src/day05_oop/Student.java
@@ -0,0 +1,21 @@
+package src.day05_oop;
+//学生类
+public class Student {
+    String name;
+    int age;
+    String sex;
+    int mathScore;
+    int englishScore;
+
+    //类中构造方法 体现封装的思想
+
+    //1.构造打印总分的函数
+    public  void totalScore(){
+        System.out.println(name + "的总成绩是：" + (mathScore + englishScore));
+    }
+
+    //2.构造打印平均分的函数
+    public  void averageScore(){
+        System.out.println(name + "的平均成绩是：" + ((mathScore + englishScore) / 2));
+    }
+}
